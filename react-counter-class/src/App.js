0075aa2230a129287c344fe15b1ee120a91b22f5
MarcelDurgante.Dev <info@marceldurgante.dev>
2022-04-09 18:14:22 +0200

    Creat React App Counter with Class

---
+++ react-counter-class/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
+    }
+
+    increment() {
+        this.setState(previousValue => ({
+            count: previousValue.count + 1
+        }));
+    }
+
+    decrement() {
+        this.setState(previousValue => ({
+            count: previousValue.count - 1
+        }));
+    }
+
+    render() {
+      return (
+          <div className="counter">
+              <h1>{this.state.count}</h1>
+              <button>+</button>
+              <button>-</button>
+          </div>
+      );
+    }
+}
+
+export default App;
